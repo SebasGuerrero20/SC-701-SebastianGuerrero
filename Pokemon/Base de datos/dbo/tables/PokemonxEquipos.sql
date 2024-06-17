eb970608f68207a6478eb107476c4aa1b63f4c17
sukencio <sebgueacu@gmail.com>
2024-06-17 10:48:39 -0600

    Base datos

---
+++ Pokemon/Base de datos/dbo/tables/PokemonxEquipos.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[PokemonxEquipos] (
+    [IdPokemon] UNIQUEIDENTIFIER NOT NULL,
+    [IdEquipo]  UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_PokemonxEquipos] PRIMARY KEY CLUSTERED ([IdPokemon] ASC, [IdEquipo] ASC)
+);
+
