557e031b9162e83c88b63b2956499d5450454848
sukencio <sebgueacu@gmail.com>
2024-07-01 12:12:15 -0600

    Commit continuacion

---
+++ Web/Abstracciones/Modelos/Entrenador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Modelos
+{
+    public class Entrenador
+    {
+        [Required (ErrorMessage ="El campo Id es requerido")]
+        public Guid? Id { get; set; }
+        [Required (ErrorMessage ="El campo Nombre es requerido")]
+        [StringLength(100, ErrorMessage =("El nombre no puede ser mayor a 100 caracteres"))]
+        [RegularExpression(@"ˆ[a-zA_Z]*$")]
+        public string? Nombre { get; set; }
+    }
+}
